56a412efbee656970c1424474ff50361c4057b5c
harishkurup <inbox30@gmail.com>
2021-02-03 19:06:33 +0530

    added routes, CRUD API

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,13 @@
 import Button from './Button';
+import { useLocation } from "react-router-dom";
 
-const Header = () => {
-    const onClick = () => {
-        console.log("CLICK!!!");
-    }
+const Header = ({onAdd, showAdd}) => {
+    const location = useLocation();
 
     return (
         <header className='header'>
             <h1>Task Tracker</h1>
-            <Button onClick={onClick} color='green' text='Add' />
+            { location.pathname === "/" && <Button onClick={onAdd} color={ showAdd ? 'red' : 'green'} text={showAdd ? 'Close': 'Add'} />}
         </header>
     )
 }
